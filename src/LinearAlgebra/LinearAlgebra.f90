fce2a6577b28749ef5683a33ccd80cf0b501d2f4
wave46 <59137482+wave46@users.noreply.github.com>
2024-09-26 13:14:22 +0200

    patched LinearAlgebra

--- src/LinearAlgebra/LinearAlgebra.f90
+++ src/LinearAlgebra/LinearAlgebra.f90
@@ -11,79 +11,79 @@ MODULE LinearAlgebra
 CONTAINS
 
   SUBROUTINE compute_lup_decomp(A, n, L, U, P)
-    integer*4, intent(in) :: n
-    real*8, intent(in) :: A(:, :)
-    real*8, intent(inout) :: L(:, :), U(:, :)
-    integer*4, intent(inout) :: P(:, :)
-    integer*4 :: i, j, itemp(1:n)
-    real*8 :: temp(1:n, 1:n)
+    INTEGER*4, INTENT(in) :: n
+    REAL*8, INTENT(in) :: A(:, :)
+    REAL*8, INTENT(inout) :: L(:, :), U(:, :)
+    INTEGER*4, INTENT(inout) :: P(:, :)
+    INTEGER*4 :: i, j, itemp(1:n)
+    REAL*8 :: temp(1:n, 1:n)
 
     temp = A
     CALL dgetrf(n, n, temp, n, itemp, i)
     L = temp
     U = temp
     DO i = 1, n
-      L(i, i) = 1.d0
-      DO j = 1, n
-        IF (i .gt. j) THEN
-          U(i, j) = 0.d0
-        ELSEIF (i .lt. j) THEN
-          L(i, j) = 0.d0
-        END IF
-      END DO
+       L(i, i) = 1.d0
+       DO j = 1, n
+          IF (i .GT. j) THEN
+             U(i, j) = 0.d0
+          ELSEIF (i .LT. j) THEN
+             L(i, j) = 0.d0
+          END IF
+       END DO
     END DO
     CALL find_permutation(n, P, itemp)
   END SUBROUTINE compute_lup_decomp
 
   SUBROUTINE find_permutation(n, P, per)
-    integer*4, intent(in) :: n
-    integer*4, intent(inout) :: per(1:n)
-    integer*4, intent(out) :: P(1:n, 1:n)
-    integer*4 :: i, temp, auxper(1:n)
+    INTEGER*4, INTENT(in) :: n
+    INTEGER*4, INTENT(inout) :: per(1:n)
+    INTEGER*4, INTENT(out) :: P(1:n, 1:n)
+    INTEGER*4 :: i, temp, auxper(1:n)
 
     P = 0
     auxper = (/(i, i=1, n)/)
 
     DO i = 1, n
-      temp = per(i)
-      per(i) = auxper(per(i))
-      auxper(temp) = auxper(i)
+       temp = per(i)
+       per(i) = auxper(per(i))
+       auxper(temp) = auxper(i)
     END DO
 
     DO i = 1, n
-      P(i, per(i)) = 1
+       P(i, per(i)) = 1
     END DO
     !  CALL check_permutation(P,A,L,U,n)
   END SUBROUTINE find_permutation
 
   SUBROUTINE check_permutation(P, A, L, U, n)
-    integer*4, intent(in) :: n, P(1:n, 1:n)
-    real*8, intent(in) :: A(1:n, 1:n), L(1:n, 1:n), U(1:n, 1:n)
-    real*8 :: temp1(1:n, 1:n), temp2(1:n, 1:n)
-    integer*4 :: i, j
+    INTEGER*4, INTENT(in) :: n, P(1:n, 1:n)
+    REAL*8, INTENT(in) :: A(1:n, 1:n), L(1:n, 1:n), U(1:n, 1:n)
+    REAL*8 :: temp1(1:n, 1:n), temp2(1:n, 1:n)
+    INTEGER*4 :: i, j
 
-    temp1 = matmul(dble(P), A)
-    temp2 = matmul(L, U)
+    temp1 = MATMUL(DBLE(P), A)
+    temp2 = MATMUL(L, U)
 
     DO i = 1, n
-      DO j = 1, n
-        IF (temp1(i, j) - temp2(i, j) .gt. 1.d-10) THEN
-          write (*, *) 'temp1-temp2 = ', temp1(i, j) - temp2(i, j), ' at i,j = ', i, j
-          write (*, *) 'P'
-          write (*, *) P(1, :)
-          write (*, *) P(2, :)
-          write (*, *) P(3, :)
-          write (*, *) 'P*A'
-          write (*, *) temp1(1, :)
-          write (*, *) temp1(2, :)
-          write (*, *) temp1(3, :)
-          write (*, *) 'L*U'
-          write (*, *) temp2(1, :)
-          write (*, *) temp2(2, :)
-          write (*, *) temp2(3, :)
-          error stop "P*A != L*U"
-        END IF
-      END DO
+       DO j = 1, n
+          IF (temp1(i, j) - temp2(i, j) .GT. 1.d-10) THEN
+             WRITE (*, *) 'temp1-temp2 = ', temp1(i, j) - temp2(i, j), ' at i,j = ', i, j
+             WRITE (*, *) 'P'
+             WRITE (*, *) P(1, :)
+             WRITE (*, *) P(2, :)
+             WRITE (*, *) P(3, :)
+             WRITE (*, *) 'P*A'
+             WRITE (*, *) temp1(1, :)
+             WRITE (*, *) temp1(2, :)
+             WRITE (*, *) temp1(3, :)
+             WRITE (*, *) 'L*U'
+             WRITE (*, *) temp2(1, :)
+             WRITE (*, *) temp2(2, :)
+             WRITE (*, *) temp2(3, :)
+             error STOP "P*A != L*U"
+          END IF
+       END DO
     END DO
 
   END SUBROUTINE check_permutation
@@ -92,17 +92,17 @@ CONTAINS
   ! Invert a square matrix
   !***************************
   SUBROUTINE invert_matrix(M, I)
-    real*8, intent(in)   :: M(:, :)
-    real*8, intent(out)  :: I(:, :)
-    integer*4            :: n
-    real*8               :: work(1:size(M, 1)) ! work array for LAPACK
-    integer*4            :: ipiv(1:size(M, 1)) ! pivot indices
-    integer*4            :: info
+    REAL*8, INTENT(in)   :: M(:, :)
+    REAL*8, INTENT(out)  :: I(:, :)
+    INTEGER*4            :: n
+    REAL*8               :: work(1:SIZE(M, 1)) ! work array for LAPACK
+    INTEGER*4            :: ipiv(1:SIZE(M, 1)) ! pivot indices
+    INTEGER*4            :: info
 
-    external DGETRF
-    external DGETRI
+    EXTERNAL DGETRF
+    EXTERNAL DGETRI
 
-    n = size(M, 1)
+    n = SIZE(M, 1)
 
     ! store the matrix in inverse to prevent it from being overwritten by LAPACK
     I = M
@@ -110,15 +110,15 @@ CONTAINS
     ! DGETRF computes an LU factorization of a general M-by-N matrix A
     ! using partial pivoting with row interchanges.
     CALL DGETRF(n, n, I, n, ipiv, info)
-    IF (info .ne. 0) THEN
-      stop 'Matrix is numerically singular!'
+    IF (info .NE. 0) THEN
+       STOP 'Matrix is numerically singular!'
     END IF
 
     ! DGETRI computes the inverse of a matrix using the LU factorization
     ! computed by DGETRF
     CALL DGETRI(n, I, n, ipiv, work, n, info)
-    IF (info .ne. 0) THEN
-      stop 'Matrix inversion failed!'
+    IF (info .NE. 0) THEN
+       STOP 'Matrix inversion failed!'
     END IF
 
   END SUBROUTINE invert_matrix
@@ -128,29 +128,29 @@ CONTAINS
   ! multiple rhs
   !***************************
   SUBROUTINE solve_linear_system(A, b, x)
-    real*8, intent(in) :: A(:, :)
-    real*8, intent(in) :: b(:, :)
-    real*8, intent(out):: x(:, :)
-    integer :: n, m, info
-    integer :: p(1:size(b, 1))
-    real*8,allocatable :: temp(:,:)
+    REAL*8, INTENT(in) :: A(:, :)
+    REAL*8, INTENT(in) :: b(:, :)
+    REAL*8, INTENT(out):: x(:, :)
+    INTEGER :: n, m, info
+    INTEGER :: p(1:SIZE(b, 1))
+    REAL*8,ALLOCATABLE :: temp(:,:)
 
-    external DGESV
+    EXTERNAL DGESV
 
-    allocate(temp(size(A, 1), size(A, 2)))
-    n = size(b, 1) ! size of the matrix (n x n)
-    m = size(b, 2) ! number of RHS
+    ALLOCATE(temp(SIZE(A, 1), SIZE(A, 2)))
+    n = SIZE(b, 1) ! size of the matrix (n x n)
+    m = SIZE(b, 2) ! number of RHS
     temp = A
     x = b
     CALL DGESV(n, m, temp, n, p, x, n, info)
 
     IF (INFO .GT. 0) THEN
-      WRITE (*, *) 'The diagonal element of the triangular factor of A,'
-      WRITE (*, *) 'U(', INFO, ',', INFO, ') is zero, so that'
-      WRITE (*, *) 'A is singular; the solution could not be computed.'
-      STOP
+       WRITE (*, *) 'The diagonal element of the triangular factor of A,'
+       WRITE (*, *) 'U(', INFO, ',', INFO, ') is zero, so that'
+       WRITE (*, *) 'A is singular; the solution could not be computed.'
+       STOP
     END IF
-    deallocate(temp)
+    DEALLOCATE(temp)
   END SUBROUTINE solve_linear_system
 
   !***************************
@@ -158,25 +158,25 @@ CONTAINS
   ! single rhs
   !***************************
   SUBROUTINE solve_linear_system_sing(A, b, x)
-    real*8, intent(in) :: A(:, :)
-    real*8, intent(in) :: b(:)
-    real*8, intent(out):: x(:)
-    integer :: n, m, info
-    integer :: p(1:size(b, 1))
-    real*8 :: temp(size(A, 1), size(A, 2))
-    external DGESV
+    REAL*8, INTENT(in) :: A(:, :)
+    REAL*8, INTENT(in) :: b(:)
+    REAL*8, INTENT(out):: x(:)
+    INTEGER :: n, m, info
+    INTEGER :: p(1:SIZE(b, 1))
+    REAL*8 :: temp(SIZE(A, 1), SIZE(A, 2))
+    EXTERNAL DGESV
 
-    n = size(b, 1) ! size of the matrix (n x n)
+    n = SIZE(b, 1) ! size of the matrix (n x n)
     m = 1 ! number of RHS
     temp = A
     x = b
     CALL DGESV(n, m, temp, n, p, x, n, info)
 
     IF (INFO .GT. 0) THEN
-      WRITE (*, *) 'The diagonal element of the triangular factor of A,'
-      WRITE (*, *) 'U(', INFO, ',', INFO, ') is zero, so that'
-      WRITE (*, *) 'A is singular; the solution could not be computed.'
-      STOP
+       WRITE (*, *) 'The diagonal element of the triangular factor of A,'
+       WRITE (*, *) 'U(', INFO, ',', INFO, ') is zero, so that'
+       WRITE (*, *) 'A is singular; the solution could not be computed.'
+       STOP
     END IF
   END SUBROUTINE solve_linear_system_sing
 
@@ -187,32 +187,32 @@ CONTAINS
   ! dgemm (avoid stack overflow)
   !***************************
   SUBROUTINE mymatmul(A,B,C)
-    real*8, intent(in) :: A(:, :)
-    real*8, intent(in) :: B(:,:)
-    real*8, intent(out):: C(:,:)
-    integer :: a1,a2,b1,b2,c1,c2
-    external DGEMM
+    REAL*8, INTENT(in) :: A(:, :)
+    REAL*8, INTENT(in) :: B(:,:)
+    REAL*8, INTENT(out):: C(:,:)
+    INTEGER :: a1,a2,b1,b2,c1,c2
+    EXTERNAL DGEMM
 
-    a1 = size(A,1)
-    a2 = size(A,2)
-    b1 = size(B,1)
-    b2 = size(B,2)
-    c1 = size(C,1)
-    c2 = size(C,2)
+    a1 = SIZE(A,1)
+    a2 = SIZE(A,2)
+    b1 = SIZE(B,1)
+    b2 = SIZE(B,2)
+    c1 = SIZE(C,1)
+    c2 = SIZE(C,2)
 
     ! Little check
-    if (a2.ne.b1) then
-      write(6,*) "Error: number of colums of A different from number of rows of B"
-      stop
-    endif
-    if (a1.ne.c1) then
-      write(6,*) "Error: number of rows of A different from number of rows of C"
-      stop
-    endif
-    if (b2.ne.c2) then
-      write(6,*) "Error: number of colums of B different from number of columns of C"
-      stop
-    endif
+    IF (a2.NE.b1) THEN
+       WRITE(6,*) "Error: number of colums of A different from number of rows of B"
+       STOP
+    ENDIF
+    IF (a1.NE.c1) THEN
+       WRITE(6,*) "Error: number of rows of A different from number of rows of C"
+       STOP
+    ENDIF
+    IF (b2.NE.c2) THEN
+       WRITE(6,*) "Error: number of colums of B different from number of columns of C"
+       STOP
+    ENDIF
 
     CALL DGEMM('N','N',a1,b2,a2,1.,A,a1,B,a2,0.,C,a1)
 
@@ -226,38 +226,38 @@ CONTAINS
   ! right eigenvectors
   !***************************
   SUBROUTINE eig(A, V, D)
-    real*8, intent(in)  :: A(:, :)
-    real*8, intent(out) :: V(:, :)
-    real*8, intent(out) :: D(:, :)
-    integer            :: m, lwork, i, info
-    Integer, Parameter :: nb = 64
-    real*8,allocatable    :: work(:)
-    real*8, allocatable :: Vmat(:, :), wr(:), wi(:)
-    real*8             :: dummy(size(A,1), size(A,1))
+    REAL*8, INTENT(in)  :: A(:, :)
+    REAL*8, INTENT(out) :: V(:, :)
+    REAL*8, INTENT(out) :: D(:, :)
+    INTEGER            :: m, lwork, i, info
+    INTEGER, PARAMETER :: nb = 64
+    REAL*8,ALLOCATABLE    :: work(:)
+    REAL*8, ALLOCATABLE :: Vmat(:, :), wr(:), wi(:)
+    REAL*8             :: dummy(SIZE(A,1), SIZE(A,1))
 
-    external dgeev
+    EXTERNAL dgeev
 
-    m = size(A, 1)
+    m = SIZE(A, 1)
 
-    allocate (wr(1:m), wi(1:m))
-    allocate (Vmat(1:m, 1:m))
+    ALLOCATE (wr(1:m), wi(1:m))
+    ALLOCATE (Vmat(1:m, 1:m))
     D = 0.d0
     Vmat = A
 
     ! Query the optimal workspace
     lwork = -1
-    call dgeev('N', 'V', m, Vmat, m, wr, wi, dummy, 1, V, m, dummy, lwork, info)
+    CALL dgeev('N', 'V', m, Vmat, m, wr, wi, dummy, 1, V, m, dummy, lwork, info)
 
     ! Compute the eigenvalues and right eigenvectors of A
-    lwork = max((nb+2)*m, nint(dummy(1,1)))
-    Allocate (work(lwork))
-    call dgeev('N', 'V', m, Vmat, m, wr, wi, dummy, 1, V, m, work, lwork, info)
+    lwork = MAX((nb+2)*m, NINT(dummy(1,1)))
+    ALLOCATE (work(lwork))
+    CALL dgeev('N', 'V', m, Vmat, m, wr, wi, dummy, 1, V, m, work, lwork, info)
 
     ! Only real eigenvalues are supposed
-    do i = 1, m
-      D(i, i) = wr(i)
-    end do
-    deallocate (wr, wi, Vmat,work)
+    DO i = 1, m
+       D(i, i) = wr(i)
+    END DO
+    DEALLOCATE (wr, wi, Vmat,work)
   END SUBROUTINE eig
 
   !*******************************************
@@ -272,9 +272,9 @@ CONTAINS
     Nl = SIZE(Vl); Nr = SIZE(Vr)
 
     DO j = 1, Nr
-      DO i = 1, Nl
-        Tens(i, j) = Vl(i)*Vr(j)
-      END DO
+       DO i = 1, Nl
+          Tens(i, j) = Vl(i)*Vr(j)
+       END DO
     END DO
   END FUNCTION TensorProduct
 
@@ -290,9 +290,9 @@ CONTAINS
     Nl = SIZE(Vl); Nr = SIZE(Vr)
 
     DO j = 1, Nr
-      DO i = 1, Nl
-        M(i, j) = M(i, j) + Vl(i)*Vr(j)
-      END DO
+       DO i = 1, Nl
+          M(i, j) = M(i, j) + Vl(i)*Vr(j)
+       END DO
     END DO
   END SUBROUTINE TensorProductCumul
 
@@ -308,9 +308,9 @@ CONTAINS
     Nl = SIZE(Vl); Nr = SIZE(Vr)
 
     DO j = 1, Nr
-      DO i = 1, Nl
-        Tens(i, j) = Vl(i) + Vr(j)
-      END DO
+       DO i = 1, Nl
+          Tens(i, j) = Vl(i) + Vr(j)
+       END DO
     END DO
   END FUNCTION TensorSumInt
 
@@ -323,10 +323,10 @@ CONTAINS
 
     INTEGER :: i, j
 
-    DO j = 1, size(M, 2)
-      DO i = 1, size(M, 1)
-        V((j - 1)*size(M, 1) + i) = M(i, j)
-      END DO
+    DO j = 1, SIZE(M, 2)
+       DO i = 1, SIZE(M, 1)
+          V((j - 1)*SIZE(M, 1) + i) = M(i, j)
+       END DO
     END DO
   END FUNCTION colint
 
@@ -339,17 +339,17 @@ CONTAINS
 
     INTEGER :: i, j
 
-    DO j = 1, size(M, 2)
-      DO i = 1, size(M, 1)
-        V((j - 1)*size(M, 1) + i) = M(i, j)
-      END DO
+    DO j = 1, SIZE(M, 2)
+       DO i = 1, SIZE(M, 1)
+          V((j - 1)*SIZE(M, 1) + i) = M(i, j)
+       END DO
     END DO
   END FUNCTION col
 END MODULE LinearAlgebra
 
 SUBROUTINE cross_product(a, b, c)
-  real*8, intent(in) :: a(3), b(3)
-  real*8, intent(out):: c(3)
+  REAL*8, INTENT(in) :: a(3), b(3)
+  REAL*8, INTENT(out):: c(3)
 
   c = 0.
   c(1) = a(2)*b(3) - a(3)*b(2)
@@ -359,21 +359,20 @@ SUBROUTINE cross_product(a, b, c)
 END SUBROUTINE cross_product
 
 SUBROUTINE ijk_cross_product(i, j, k)
-  integer, intent(in) :: i
-  integer, intent(out):: j, k
+  INTEGER, INTENT(in) :: i
+  INTEGER, INTENT(out):: j, k
 
   SELECT CASE (i)
   CASE (1)
-    j = 3
-    k = 2
+     j = 3
+     k = 2
   CASE (2)
-    j = 1
-    k = 3
+     j = 1
+     k = 3
   CASE (3)
-    j = 2
-    k = 1
+     j = 2
+     k = 1
   CASE DEFAULT
-    WRITE (*, *) "Error, wrong case in cross-product"
+     WRITE (*, *) "Error, wrong case in cross-product"
   END SELECT
 END SUBROUTINE ijk_cross_product
-
