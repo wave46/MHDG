74026de38ad11a4c7492a7c81e4339b9c3a7186e
wave46 <59137482+wave46@users.noreply.github.com>
2024-09-26 11:01:43 +0200

    patched prec_const.f90

--- src/Definitions/prec_const.f90
+++ src/Definitions/prec_const.f90
@@ -5,17 +5,16 @@
 ! Definition of real precision and real constants
 !********************************************
 
-module prec_const
-  implicit none
+MODULE prec_const
+  IMPLICIT NONE
 
   ! Precision for real
   !*****************
-  integer, parameter :: float = SELECTED_REAL_KIND(13, 99)
+  INTEGER, PARAMETER :: float = SELECTED_REAL_KIND(13, 99)
 
   ! Some useful constants
   !*********************
   REAL(float), PARAMETER :: PI = 3.141592653589793238462643383279502884197_float
   REAL(float), PARAMETER :: sqrt2 = 1.414213562373095_float
 
-end module prec_const
-
+END MODULE prec_const
