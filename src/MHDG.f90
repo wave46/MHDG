8c772650a1f7981d398b81db5c2deca3b1b9bbab
wave46 <59137482+wave46@users.noreply.github.com>
2025-03-03 12:47:33 +0100

    Removing read_gmsh from MHDG
    We now run only from gmsh meshes and the mesh will be saved always in solution
    
    Now either read_gmsh or readMeshFromSol should be true anyway

--- src/MHDG.f90
+++ src/MHDG.f90
@@ -83,7 +83,7 @@ PROGRAM MHDG
   ! used in the HDG scheme
   ierr = 1
 #ifdef PARALL
-  IF((switch%read_gmsh) .OR. (switch%readMeshFromSol) .AND. (MPIvar%glob_size .GT. 1)) THEN
+  IF((MPIvar%glob_size .GT. 1)) THEN
      CALL mesh_preprocess_serial(ierr)
   ELSE
      CALL mesh_preprocess(ierr)
