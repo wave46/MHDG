a1e38be102a6065103474fe47601fca9ed78d57f
wave46 <59137482+wave46@users.noreply.github.com>
2025-03-10 17:02:04 +0100

    unused variables in compute_sigmavrec

--- src/Models/NGammaTiTe/physics.f90
+++ src/Models/NGammaTiTe/physics.f90
@@ -1804,10 +1804,8 @@ CONTAINS
   ENDSUBROUTINE compute_dsigmavnn_dU
   SUBROUTINE compute_sigmavrec(U,sigmavrec)
     real*8, intent(IN) :: U(:)
-    real*8             :: sigmavrec,U1,U4,T0,Ery,E0,te,ne,n0
-    real*8, dimension(9,9) :: alpha
+    real*8             :: sigmavrec,U1,U4,T0,te,ne,n0
     real*8, parameter    :: tol = 1.e-20  !tolerance for U4 = 3/2*Mref*U1min*te_min/T0
-    integer            :: i, j
     U1 = U(1)
     U4 = U(4)
     T0 = 50.
