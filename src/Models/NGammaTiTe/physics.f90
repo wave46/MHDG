9b7d8f1341c2d3a99307698bdf1f5fdd5e24a670
wave46 <59137482+wave46@users.noreply.github.com>
2025-03-10 16:58:03 +0100

    unused variable

--- src/Models/NGammaTiTe/physics.f90
+++ src/Models/NGammaTiTe/physics.f90
@@ -1865,7 +1865,7 @@ CONTAINS
 
   SUBROUTINE compute_dsigmavErec_dU(U,res)
     real*8, intent(IN) :: U(:)
-    real*8             :: res(:),sigmavErec,U1,U4,T0,te,ne,n0
+    real*8             :: res(:),U1,U4,T0,te,ne,n0
     REAL*8, PARAMETER    :: tol = 1.e-20 !tolerance for U4 = 3/2*Mref*U1min*te_min/T0
     U1 = U(1)
     U4 = U(4)
