b10ce413fe6f541d7faa9709d6830e0f8071e528
wave46 <59137482+wave46@users.noreply.github.com>
2025-03-10 17:04:35 +0100

    unused variables in compute_sigmavEiz

--- src/Models/NGammaTiTe/physics.f90
+++ src/Models/NGammaTiTe/physics.f90
@@ -1706,10 +1706,8 @@ CONTAINS
 
   SUBROUTINE compute_sigmavEiz(U,sigmavEiz)
     real*8, intent(IN) :: U(:)
-    real*8             :: sigmavEiz,U1,U4,T0,Ery,E0,te,ne,n0
-    real*8, dimension(9,9) :: alpha
+    real*8             :: sigmavEiz,U1,U4,T0,te,ne,n0
     REAL*8, PARAMETER    :: tol = 1.e-20  !tolerance for U4 = 3/2*Mref*U1min*te_min/T0
-    integer            :: i, j
     U1 = U(1)
     U4 = U(4)
     T0 = 50.
