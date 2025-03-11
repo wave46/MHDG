36ed68dd8af69aa12bda751137a0de16073eee68
wave46 <59137482+wave46@users.noreply.github.com>
2025-03-11 15:55:22 +0100

    Adding plug routine to replace the adaptivity call

--- src/Adaptivity/adaptivity_common.f90
+++ src/Adaptivity/adaptivity_common.f90
@@ -14,6 +14,44 @@ MODULE adaptivity_common_module
 
 CONTAINS
 
+  SUBROUTINE adaptivity_new(mesh_name,count_adapt,restart_adapt)
+
+   ! mesh_name is the mesh path + mesh name + .msh extension ("./Meshes/CircLim.msh")
+   ! mesh_name_npne (mesh name no path no extension) is just the name of the mesh ("CircLim")
+   ! new_mesh_name_npne (new mesh name no path no extension) is just the name of the mesh + param_adapt + count_adapt ("CircLim_param2_n1")
+   ! buffer is a dummy array to store intermediate mesh names
+   CHARACTER(1024), INTENT(IN)                 :: mesh_name
+   INTEGER, INTENT(IN)                         :: count_adapt
+   LOGICAL, INTENT(IN)                         :: restart_adapt
+   
+
+
+   CALL adaptivity_console_output(restart_adapt)
+
+   ENDSUBROUTINE adaptivity_new
+
+   SUBROUTINE adaptivity_console_output(restart_adapt)
+      LOGICAL, INTENT(IN) :: restart_adapt
+      CHARACTER(1024) :: buffer
+
+      IF ((adapt%evaluator .EQ. 2)) THEN
+         buffer = "            ADAPTIVITY ESTIMATOR                 "
+      ELSEIF ((adapt%evaluator .EQ. 1)) THEN
+         buffer = "            ADAPTIVITY INDICATOR                 "
+      ELSEIF((adapt%evaluator .EQ. 0) .OR. (restart_adapt)) THEN
+         buffer = "        ADAPTIVITY ESTIMATOR-INDICATOR           "
+      ENDIF
+
+      IF(MPIvar%glob_id .EQ. 0) THEN
+         WRITE(*,*) "*************************************************"
+         WRITE(*,*) TRIM(buffer)
+         WRITE(*,*) "*************************************************"
+      ENDIF
+
+   ENDSUBROUTINE adaptivity_console_output
+
+
+
   SUBROUTINE merge_with_geometry(gmsh_l)
     TYPE(gmsh_t), INTENT(IN)          :: gmsh_l
 
