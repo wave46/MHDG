2e065c75fa6dc72ac24fef6a488e0670e033c690
wave46 <59137482+wave46@users.noreply.github.com>
2025-03-12 17:29:15 +0100

    Removing adaptovity_indicator_estimator file and its dependencies

--- src/Utils/Main_utils.f90
+++ src/Utils/Main_utils.f90
@@ -20,7 +20,6 @@ MODULE Main_utils
   USE adaptivity_common_module
   USE adaptivity_estimator_module
   USE adaptivity_indicator_module
-  USE adaptivity_estimator_indicator_module
   USE adaptivity_general_module
   USE Postprocess, only: computeL2ErrorAnalyticSol
 #ifdef PARALL
