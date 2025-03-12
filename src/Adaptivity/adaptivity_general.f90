f13b0b2b67cd988fedb8af625a327d6e1917f01c
wave46 <59137482+wave46@users.noreply.github.com>
2025-03-12 14:53:49 +0100

    Added parallel functionality to new adaptivity call

--- src/Adaptivity/adaptivity_general.f90
+++ src/Adaptivity/adaptivity_general.f90
@@ -4,17 +4,27 @@ MODULE adaptivity_general_module
     USE adaptivity_indicator_module
     USE adaptivity_estimator_module
     USE adaptivity_estimator_indicator_module
+    USE MPI_OMP
 
 CONTAINS
 
     SUBROUTINE adaptivity_new(mesh_name,count_adapt,order)
+#ifdef PARALL
+        USE Communications, only: gather_connectivity, gather_elemental_values
+#endif
         CHARACTER(1024), INTENT(IN)                 :: mesh_name
         INTEGER, INTENT(IN)                         :: count_adapt
         INTEGER, INTENT(IN)                         :: order
         REAL*8                                      :: h_map_elements(SIZE(Mesh%T,1))
         REAL*8                                      :: h_target_elements(SIZE(Mesh%T,1))
-        REAL*8, ALLOCATABLE                             :: h_target_vertices(:)
-
+        REAL*8, POINTER                             :: h_target_vertices(:)
+#ifdef PARALL
+        INTEGER                                     :: ierr
+        INTEGER,POINTER                              :: T_global(:,:)
+        REAL*8,POINTER                              :: h_target_elements_global(:)
+        NULLIFY(T_global,h_target_elements_global)
+#endif
+        NULLIFY(h_target_vertices)
         
         
         
@@ -33,20 +43,37 @@ CONTAINS
         ENDIF
 
 #ifdef PARALL
+        CALL gather_connectivity(Mesh,T_global,allgather=.false.)
+        CALL gather_elemental_values(Mesh,h_target_elements,h_target_elements_global,allgather=.false.)
         IF(MPIvar%glob_id .EQ. 0) THEN
+            CALL get_h_target_vertices(h_target_elements_global,h_target_vertices,T_global)
+#else
+            CALL get_h_target_vertices(h_target_elements,h_target_vertices,Mesh%T)
 #endif
 
-        CALL get_h_target_vertices(h_target_elements,h_target_vertices)
-
-        CALL generate_new_mesh(mesh_name,h_target_vertices,count_adapt)
-
+            CALL generate_new_mesh(mesh_name,h_target_vertices,count_adapt)
 #ifdef PARALL
         ENDIF
+        CALL MPI_BARRIER(MPI_COMM_WORLD,ierr)
 #endif
         CALL load_new_mesh(order)
-        IF (ALLOCATED(h_target_vertices)) THEN
-           DEALLOCATE(h_target_vertices)        
+
+
+
+        IF (ASSOCIATED(h_target_vertices)) THEN
+           DEALLOCATE(h_target_vertices)
+           NULLIFY(h_target_vertices)        
         ENDIF
+#ifdef PARALL
+        IF (ASSOCIATED(T_global)) THEN
+           DEALLOCATE(T_global)
+           NULLIFY(T_global)
+        ENDIF
+        IF (ASSOCIATED(h_target_elements_global)) THEN
+           DEALLOCATE(h_target_elements_global)
+           NULLIFY(h_target_elements_global)
+        ENDIF
+#endif
 
     ENDSUBROUTINE adaptivity_new
 
