0451acfa78c4b2fc80beeac2527521051e2e8470
wave46 <wave880@gmail.com>
2025-04-07 13:58:07 +0200

    Removing mmg from arch make and source file

--- lib/Make.inc/init_vars_libs_meso.sh
+++ lib/Make.inc/init_vars_libs_meso.sh
@@ -29,7 +29,6 @@ export MHDG_LIB_DIR=$HOME/local
 export MHDG_PASTIX_DIR=$MHDG_LIB_DIR/pastix_32
 source $MHDG_PASTIX_DIR/bin/pastix_env.sh
 export MHDG_SCOTCH_DIR=$MHDG_LIB_DIR/scotch_32
-export MHDG_MMG_DIR=$HOME/libs/mmg
 export MHDG_GMSH_DIR=$MHDG_LIB_DIR/gmsh_1.11_iopenmpi_2020
 
 echo "Libraries directory: $MHDG_LIB_DIR"
@@ -40,5 +39,4 @@ echo "PASTIX (https://gitlab.inria.fr/solverstack/pastix) directory: $MHDG_PASTI
 #echo "PSBLAS (https://github.com/sfilippone/psblas3) directory: $MHDG_PSBLAS_DIR"
 #echo "MLD2P4 (https://github.com/sfilippone/mld2p4-2) directory: $MHDG_MLD2P4_DIR"
 #echo "PETSC directory: $MHDG_PETSC_DIR"
-echo "MMG directory: $MHDG_MMG_DIR"
 echo "GMSH directory: $MHDG_GMSH_DIR"
