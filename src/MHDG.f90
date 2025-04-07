c88e85261e41b77639e0ea0b843ff5792327612f
wave46 <wave880@gmail.com>
2025-04-07 14:00:55 +0200

    Fixing restart from solution in MHDG, to handle both serial and parallel

--- src/MHDG.f90
+++ src/MHDG.f90
@@ -117,15 +117,29 @@ PROGRAM MHDG
   ENDIF
 #endif
 
-  ! initialise magnetic field (the mesh is needed)
-  CALL initialize_magnetic_field()
-  ! load magnetic field and Jtor
-  CALL load_magnetic_field_Jtor()
 
-  ! if the restart solution is not given or we are in the serial environment
-  IF ((nb_args .EQ. 1) .OR. (MPIvar%glob_size .EQ. 1)) THEN
+  ! if the restart solution is not given
+  IF ((nb_args .EQ. 1)) THEN
+     ! initialise magnetic field (the mesh is needed)
+     CALL initialize_magnetic_field()
+     ! load magnetic field and Jtor
+     CALL load_magnetic_field_Jtor()
+     ! initialise solution
      CALL initialize_solution()
+  ELSEIF ((MPIvar%glob_size .EQ. 1)) THEN
+       ! load the serial solution
+       CALL initialize_solution()
+       ! initialise magnetic field (the mesh is needed)
+       CALL initialize_magnetic_field()
+       ! load magnetic field and Jtor
+       CALL load_magnetic_field_Jtor()
+  ELSE 
+      ! initialise magnetic field (the mesh is needed)
+      CALL initialize_magnetic_field()
+      ! load magnetic field and Jtor
+      CALL load_magnetic_field_Jtor()
   ENDIF
+  
 
   restart_adapt = adapt%rest_adapt
   CALL set_parameters()
