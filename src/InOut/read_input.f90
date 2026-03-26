cf0011ca719aeaddc15d7da24ce6047a1cde5fc8
wave46 <59137482+wave46@users.noreply.github.com>
2026-03-26 11:17:57 +0100

    Wire transport-model settings reader
    Read the dedicated transport-model namelist from the main input path.\nExpose the configured rho_core and rho_edge in the run summary for easier postprocessing checks.

--- src/InOut/read_input.f90
+++ src/InOut/read_input.f90
@@ -44,6 +44,7 @@ SUBROUTINE READ_input()
 
   ! Info for input and output
   CHARACTER(len = 1000) :: field_path, jtor_path,save_folder, geometry_path,puff_path, target_density_path,target_density_xpr_path, impurity_concentration_path, zeff_path
+  CHARACTER(len = 1000) :: transport_model_path = 'transport_model.nml'
   INTEGER               :: field_dimensions(1:2), jtor_dimensions(1:2),puff_dimension, target_density_dimension,target_density_xpr_dimension,impurity_concentration_dimension, zeff_dimension
   LOGICAL               :: field_from_grid, compute_from_flux, divide_by_2pi
 
@@ -93,7 +94,7 @@ SUBROUTINE READ_input()
   NAMELIST /SWITCH_LST/ steady,read_gmsh, readMeshFromSol, set_2d_order, order_2d, gmsh2h5, axisym,external_heating, impurity_radiation, init, driftdia, driftexb, testcase, OhmicSrc, ME,diff_reverse_Ip, target_variable, RMP, Ripple, psdtime, diffred, diffmin, &
        & shockcp, limrho, difcor, thresh, filter, decoup, ckeramp, saveNR, saveTau, transport_1d, fixdPotLim, dirivortcore,dirivortlim, convvort,pertini,&
        & logrho,bxgradb,flux_limiter,import_diffusion_1D,bohm_gyrobohm
-  NAMELIST /INPUT_LST/ field_path, field_dimensions,field_from_grid,compute_from_flux,divide_by_2pi, jtor_path, jtor_dimensions,external_heating_path,external_heating_from_grid, save_folder,puff_path,puff_dimension,target_density_path,target_density_dimension,target_density_xpr_path,target_density_xpr_dimension,impurity_concentration_path,impurity_concentration_dimension,zeff_path,zeff_dimension, diffusion_1D_path
+  NAMELIST /INPUT_LST/ field_path, field_dimensions,field_from_grid,compute_from_flux,divide_by_2pi, jtor_path, jtor_dimensions,external_heating_path,external_heating_from_grid, save_folder,puff_path,puff_dimension,target_density_path,target_density_dimension,target_density_xpr_path,target_density_xpr_dimension,impurity_concentration_path,impurity_concentration_dimension,zeff_path,zeff_dimension, diffusion_1D_path, transport_model_path
   NAMELIST /NUMER_LST/ tau,nrp,tNR,tTM,div,sc_coe,sc_sen,minrho,so_coe,df_coe,dc_coe,thr,thrpre,stab,dumpnr_min,dumpnr_max,dumpnr_width,dumpnr_n0,ntor,ptor,tmax,npartor,bohmtypebc,exbdump
   NAMELIST /ADAPT_LST/ adaptivity,shockcp_adapt, evaluator, param_est, thr_ind, quant_ind, n_quant_ind,tol_est, difference, time_adapt, NR_adapt, freq_t_adapt, freq_NR_adapt, div_adapt, rest_adapt, osc_adapt, osc_tol, osc_check, geometry_path
   NAMELIST /GEOM_LST/ R0, q
@@ -211,6 +212,7 @@ SUBROUTINE READ_input()
   input%zeff_dimension    = zeff_dimension
   input%puff_dimension   = puff_dimension
   input%diffusion_1D_path = TRIM(ADJUSTL(diffusion_1D_path))
+  input%transport_model_path = TRIM(ADJUSTL(transport_model_path))
   numer%tau               = tau
   numer%nrp               = nrp
   numer%tNR               = tNR
@@ -258,6 +260,8 @@ SUBROUTINE READ_input()
   adapt%osc_adapt         = osc_adapt
   adapt%osc_tol           = osc_tol
   adapt%osc_check         = osc_check
+
+  IF (switch%transport_1d) CALL read_transport_model_input()
   adapt%geometry_path     = TRIM(ADJUSTL(geometry_path))
   geom%R0                 = R0
   geom%q                  = q
@@ -568,6 +572,9 @@ SUBROUTINE READ_input()
      PRINT *, '                - saveNR:                                             ', saveNR
      PRINT *, '                - saveTau:                                            ', saveTau
      PRINT *, '                - transport_1d:                          ', transport_1d
+     PRINT *, '                - transport_model_path:                  ', TRIM(ADJUSTL(input%transport_model_path))
+     PRINT *, '                - rho_core (transport model):            ', transport_model_input%rho_core
+     PRINT *, '                - rho_edge (transport model):            ', transport_model_input%rho_edge
      PRINT *, '        ***************** Numerics ****************************'
      PRINT *, '                - stabilization type:                                 ', numer%stab
      PRINT *, '                - tau(1):                                             ', numer%tau(1)
@@ -662,4 +669,37 @@ SUBROUTINE READ_input()
 
      PRINT *, '        '
   END IF
+
 END SUBROUTINE READ_input
+
+SUBROUTINE read_transport_model_input()
+  USE globals
+  USE MPI_OMP
+  IMPLICIT NONE
+
+  INTEGER :: utransport, ios
+  REAL*8 :: rho_core, rho_edge
+  NAMELIST /TRANSPORT_MODEL_1D_LST/ rho_core, rho_edge
+
+  rho_core = transport_model_input%rho_core
+  rho_edge = transport_model_input%rho_edge
+
+  IF (LEN_TRIM(input%transport_model_path) == 0) RETURN
+
+  utransport = 101
+  OPEN(utransport, file=TRIM(ADJUSTL(input%transport_model_path)), status='old', iostat=ios)
+  IF (ios /= 0) THEN
+     IF (MPIvar%glob_id == 0) WRITE(6,*) 'Warning: could not open transport model settings file: ', TRIM(ADJUSTL(input%transport_model_path))
+     RETURN
+  END IF
+
+  READ(utransport, nml=TRANSPORT_MODEL_1D_LST, iostat=ios)
+  CLOSE(utransport)
+  IF (ios /= 0) THEN
+     IF (MPIvar%glob_id == 0) WRITE(6,*) 'Warning: could not read TRANSPORT_MODEL_1D_LST from file: ', TRIM(ADJUSTL(input%transport_model_path))
+     RETURN
+  END IF
+
+  transport_model_input%rho_core = rho_core
+  transport_model_input%rho_edge = rho_edge
+END SUBROUTINE read_transport_model_input
