1b8c810d95e233d4fa0b08c7cb830987e3b5600c
mcapassox <marcello.capasso@gmail.com>
2026-07-15 16:44:16 +0200

    mainly formatting and removing of unused modules and variables

--- src/LinearAlgebra/solve_pastix.f90
+++ src/LinearAlgebra/solve_pastix.f90
@@ -1,47 +1,47 @@
 MODULE solve_pastix
-   use iso_c_binding
-   use spmf
-   use pastixf
+   USE iso_c_binding
+   USE spmf
+   USE pastixf
    USE matrices_types
    USE types
    USE globals
+   USE MPI_OMP, ONLY: MPIvar, OMPvar
+
    IMPLICIT NONE
 
    TYPE PASTIX_STRUC ! A type to store matrices in CSR format
       ! Variables are defined in a PASTIX specific way
-      integer                                               :: n        ! Number of vertices
+      INTEGER                                               :: n        ! Number of vertices
       ! integer                                               :: nnz      ! Number of non-zeroes in the graph
       ! integer(kind=pastix_int_t), dimension(:), allocatable :: rows     ! Array of size n+1 of indirections to rows for each vertex
       ! integer(kind=pastix_int_t), dimension(:), allocatable :: cols     ! Array of size nnz that corresponds to the global column numbering
       ! integer(kind=pastix_int_t), dimension(:), allocatable :: loc2glob ! Corresponding numbering from local to global
       ! real(kind=c_double),        dimension(:), allocatable :: vals     ! Values stored in the matrix
       ! From here below the variables are PASTIX specific
-      type(pastix_data_t), pointer                      :: pastix_data
+      type(pastix_data_t), POINTER                      :: pastix_data => NULL()
 
-      type(spmatrix_t), pointer :: spm      ! Spm format of the matrix
+      type(spmatrix_t), POINTER :: spm => NULL()      ! Spm format of the matrix
       ! integer                               :: pastix_comm ! MPI communicator used by pastix
       ! pastix_int_t, dimension(:), pointer :: perm => null() ! permutation tabular
       ! pastix_int_t, dimension(:), pointer :: invp => null() ! reverse permutation tabular
       ! pastix_float_t, dimension(:), pointer :: rhs => null() ! Right hand side
-      integer(c_int)                                           :: info
-      integer(kind=pastix_int_t)                               :: nrhs
-      real(kind=c_double), dimension(:, :), allocatable :: x, b
-      real(kind=c_double), dimension(:), allocatable :: rhs
-      integer(kind=pastix_int_t)                       :: iparm(iparm_size)
-      real(kind=c_double)                              :: dparm(dparm_size)
+      INTEGER(c_int)                                           :: info
+      INTEGER(kind=pastix_int_t)                               :: nrhs
+      REAL(kind=c_double), DIMENSION(:, :), ALLOCATABLE :: x, b
+      REAL(kind=c_double), DIMENSION(:), ALLOCATABLE :: rhs
+      INTEGER(kind=pastix_int_t)                       :: iparm(iparm_size)
+      REAL(kind=c_double)                              :: dparm(dparm_size)
 
-      ! Integer                               :: nbthread = OMPvar%Nthreads    ! Number of threads in PaStiX
    END TYPE
 
-   TYPE(PASTIX_STRUC) :: matPASTIX
+   TYPE(PASTIX_STRUC)           :: matPASTIX
 
 CONTAINS
 
    SUBROUTINE set_nthreads(matPASTIX)
-      use MPI_OMP, only: OMPvar
-      TYPE(PASTIX_STRUC) :: matPASTIX
+      TYPE(PASTIX_STRUC)        :: matPASTIX
       matPASTIX%iparm(IPARM_THREAD_NBR) = OMPvar%Nthreads
-   end SUBROUTINE set_nthreads
+   END SUBROUTINE set_nthreads
 
    !***********************************************
    ! Initialization of matrix
@@ -51,27 +51,25 @@ CONTAINS
 
       IMPLICIT NONE
 
-      !   TYPE(MAT_CSR_TYP) :: matCSR
-      TYPE(PASTIX_STRUC) :: matPASTIX
+      !   TYPE(MAT_CSR_TYP)     :: matCSR
+      TYPE(PASTIX_STRUC)        :: matPASTIX
 
-      real*8  :: tps, tpe
-      integer :: cks, clock_rate, cke
+      REAL*8                    :: tps, tpe
+      INTEGER                   :: cks, clock_rate, cke
       cks = 0.
 
-      if (lssolver%timing) then
-         call cpu_time(tps)
-         call system_clock(cks, clock_rate)
-      end if
+      IF (lssolver%timing) THEN
+         CALL cpu_time(tps)
+         CALL system_clock(cks, clock_rate)
+      END IF
 
       matPASTIX%n = matK%n
 
       matPASTIX%nrhs = 1
 
-      call pastixInitParam(matPASTIX%iparm, matPASTIX%dparm)
+      CALL pastixInitParam(matPASTIX%iparm, matPASTIX%dparm)
 
-      call set_nthreads(matPASTIX)
-
-
+      CALL set_nthreads(matPASTIX)
 
       ! matPASTIX%iparm(IPARM_SYM) = API_SYM_NO
       ! Verbose mode - Default: PastixVerboseNo
@@ -99,17 +97,17 @@ CONTAINS
       ! We solve A^T x = b because of the csr/csc format
       matPASTIX%iparm(IPARM_TRANSPOSE_SOLVE) = PastixTrans
 
-      call pastixInit(matPASTIX%pastix_data, MPI_COMM_WORLD, matPASTIX%iparm, matPASTIX%dparm)
-      allocate (matPASTIX%spm)
-      call spmInitDist(matPASTIX%spm, MPI_COMM_WORLD)
-      call build_mat_PASTIX(matPASTIX)
+      CALL pastixInit(matPASTIX%pastix_data, MPI_COMM_WORLD, matPASTIX%iparm, matPASTIX%dparm)
+      ALLOCATE(matPASTIX%spm)
+      CALL spmInitDist(matPASTIX%spm, MPI_COMM_WORLD)
+      CALL build_mat_PASTIX(matPASTIX)
 
-      if (lssolver%timing) then
-         call cpu_time(tpe)
-         call system_clock(cke, clock_rate)
-         timing%rlstime1 = timing%rlstime1 + (cke - cks)/real(clock_rate)
+      IF (lssolver%timing) THEN
+         CALL cpu_time(tpe)
+         CALL system_clock(cke, clock_rate)
+         timing%rlstime1 = timing%rlstime1 + (cke - cks)/REAL(clock_rate)
          timing%clstime1 = timing%clstime1 + tpe - tps
-      end if
+      END IF
    END SUBROUTINE init_mat_PASTIX
 
    !***********************************************
@@ -118,26 +116,26 @@ CONTAINS
    !***********************************************
    SUBROUTINE build_mat_PASTIX(matPASTIX)
 
-      TYPE(PASTIX_STRUC) :: matPASTIX
-      real(kind=c_double)                                      :: normA
-      real*8  :: tps, tpe
-      integer :: cks, clock_rate, cke
+      TYPE(PASTIX_STRUC)        :: matPASTIX
+      REAL(kind=c_double)                                      :: normA
+      REAL*8                    :: tps, tpe
+      INTEGER                   :: cks, clock_rate, cke, ierr
       cks =0.
 
-      if (lssolver%timing) then
-         call cpu_time(tps)
-         call system_clock(cks, clock_rate)
-      end if
+      IF (lssolver%timing) THEN
+         CALL cpu_time(tps)
+         CALL system_clock(cks, clock_rate)
+      END IF
 
       ! we deallocate and reallocate because the mesh can change
-      if (associated(matPASTIX%spm)) then
+      IF (associated(matPASTIX%spm)) THEN
          matPASTIX%spm%colptr = c_null_ptr
          matPASTIX%spm%rowptr = c_null_ptr
          matPASTIX%spm%values = c_null_ptr
          matPASTIX%spm%loc2glob = c_null_ptr
-         call spmExit(matPASTIX%spm)
+         CALL spmExit(matPASTIX%spm)
          !deallocate (matPASTIX%spm)
-      end if
+      END IF
 
       !allocate (matPASTIX%spm)
 
@@ -158,7 +156,8 @@ CONTAINS
       matPASTIX%spm%replicated = 1
 #endif
 
-      call spmUpdateComputedFields(matPASTIX%spm)
+      CALL MPI_BARRIER(MPI_COMM_WORLD, ierr)
+
       !call spmAlloc(matPASTIX%spm)
 
       !call spmGetArray(matPASTIX%spm, colptr=colptr, rowptr=rowptr, dvalues=values)
@@ -170,32 +169,34 @@ CONTAINS
       matPASTIX%spm%values = c_loc(matK%vals)
       matPASTIX%spm%loc2glob = c_loc(matK%loc2glob)
 
-      ! Scale A for better stability with low-rank computations
-      call spmNorm(SpmFrobeniusNorm, matPASTIX%spm, normA)
-      call spmScal(1./normA, matPASTIX%spm)
+      CALL spmUpdateComputedFields(matPASTIX%spm)
 
-      if (allocated(matPASTIX%x)) then
-         deallocate (matPASTIX%x)
-      end if
-      if (allocated(matPASTIX%b)) then
-         deallocate (matPASTIX%b)
-      end if
-      if (allocated(matPASTIX%rhs)) then
-         deallocate (matPASTIX%rhs)
-      end if
-      allocate (matPASTIX%x(matPASTIX%spm%nexp, matPASTIX%nrhs))
-      allocate (matPASTIX%b(matPASTIX%spm%nexp, matPASTIX%nrhs))
-      allocate (matPASTIX%rhs(matPASTIX%spm%nexp))
+      ! Scale A for better stability with low-rank computations
+      CALL spmNorm(SpmFrobeniusNorm, matPASTIX%spm, normA)
+      CALL spmScal(1./normA, matPASTIX%spm)
+
+      IF (allocated(matPASTIX%x)) THEN
+         DEALLOCATE (matPASTIX%x)
+      END IF
+      IF (allocated(matPASTIX%b)) THEN
+         DEALLOCATE (matPASTIX%b)
+      END IF
+      IF (allocated(matPASTIX%rhs)) THEN
+         DEALLOCATE (matPASTIX%rhs)
+      END IF
+      ALLOCATE (matPASTIX%x(matPASTIX%spm%nexp, matPASTIX%nrhs))
+      ALLOCATE (matPASTIX%b(matPASTIX%spm%nexp, matPASTIX%nrhs))
+      ALLOCATE (matPASTIX%rhs(matPASTIX%spm%nexp))
 
       matPASTIX%b(:, 1) = rhs%vals/normA
       matPASTIX%x(:, 1) = matPASTIX%b(:, 1)
 
-      if (lssolver%timing) then
-         call cpu_time(tpe)
-         call system_clock(cke, clock_rate)
-         timing%rlstime4 = timing%rlstime4 + (cke - cks)/real(clock_rate)
+      IF (lssolver%timing) THEN
+         CALL cpu_time(tpe)
+         CALL system_clock(cke, clock_rate)
+         timing%rlstime4 = timing%rlstime4 + (cke - cks)/REAL(clock_rate)
          timing%clstime4 = timing%clstime4 + tpe - tps
-      end if
+      END IF
    END SUBROUTINE build_mat_PASTIX
 
    !***********************************
@@ -204,61 +205,60 @@ CONTAINS
    !***********************************
    SUBROUTINE check_mat_PASTIX(matPASTIX)
 
-      TYPE(PASTIX_STRUC) :: matPASTIX
-      type(spmatrix_t), pointer                                 :: spm2
+      TYPE(PASTIX_STRUC)        :: matPASTIX
+      type(spmatrix_t), POINTER                                 :: spm2
 
-      real*8  :: tps, tpe
-      integer :: cks, clock_rate, cke
+      REAL*8                    :: tps, tpe
+      INTEGER                   :: cks, clock_rate, cke
       cks = 0.
 
-      if (lssolver%timing) then
-         call cpu_time(tps)
-         call system_clock(cks, clock_rate)
-      end if
+      IF (lssolver%timing) THEN
+         CALL cpu_time(tps)
+         CALL system_clock(cks, clock_rate)
+      END IF
 
-      allocate (spm2)
-      call spmCheckAndCorrect(matPASTIX%spm, spm2, matPASTIX%info)
-      if (matPASTIX%info .ne. 0) then
-         call spmExit(matPASTIX%spm)
+      ALLOCATE (spm2)
+      CALL spmCheckAndCorrect(matPASTIX%spm, spm2, matPASTIX%info)
+      IF (matPASTIX%info .NE. 0) THEN
+         CALL spmExit(matPASTIX%spm)
          matPASTIX%spm = spm2
-      end if
-      deallocate (spm2)
+      END IF
+      DEALLOCATE (spm2)
 
-      if (lssolver%timing) then
-         call cpu_time(tpe)
-         call system_clock(cke, clock_rate)
-         timing%rlstime2 = timing%rlstime2 + (cke - cks)/real(clock_rate)
+      IF (lssolver%timing) THEN
+         CALL cpu_time(tpe)
+         CALL system_clock(cke, clock_rate)
+         timing%rlstime2 = timing%rlstime2 + (cke - cks)/REAL(clock_rate)
          timing%clstime2 = timing%clstime2 + tpe - tps
-      end if
+      END IF
    END SUBROUTINE check_mat_PASTIX
 
    !***********************************************
    ! Analysis of matrix with PASTIX
    !***********************************************
    SUBROUTINE anal_mat_PASTIX(matPASTIX)
-      use matrices_tools, only: dump_CSR
 
-      TYPE(PASTIX_STRUC) :: matPASTIX
-      real*8  :: tps, tpe
-      integer :: cks, clock_rate, cke
+      TYPE(PASTIX_STRUC)        :: matPASTIX
+      REAL*8                    :: tps, tpe
+      INTEGER                   :: cks, clock_rate, cke
       cks = 0.
 
-      if (lssolver%timing) then
-         call cpu_time(tps)
-         call system_clock(cks, clock_rate)
-      end if
+      IF (lssolver%timing) THEN
+         CALL cpu_time(tps)
+         CALL system_clock(cks, clock_rate)
+      END IF
 
-      call pastix_task_analyze(matPASTIX%pastix_data, matPASTIX%spm, matPASTIX%info)
-      if (matPASTIX%info .ne. 0) then
-         error stop "pastix_task_analyze failed"
-      end if
+      CALL pastix_task_analyze(matPASTIX%pastix_data, matPASTIX%spm, matPASTIX%info)
+      IF (matPASTIX%info .NE. 0) THEN
+         error STOP "pastix_task_analyze failed"
+      END IF
 
-      if (lssolver%timing) then
-         call cpu_time(tpe)
-         call system_clock(cke, clock_rate)
-         timing%rlstime3 = timing%rlstime3 + (cke - cks)/real(clock_rate)
+      IF (lssolver%timing) THEN
+         CALL cpu_time(tpe)
+         CALL system_clock(cke, clock_rate)
+         timing%rlstime3 = timing%rlstime3 + (cke - cks)/REAL(clock_rate)
          timing%clstime3 = timing%clstime3 + tpe - tps
-      end if
+      END IF
    END SUBROUTINE anal_mat_PASTIX
 
    !***********************************************
@@ -266,28 +266,28 @@ CONTAINS
    !***********************************************
    SUBROUTINE LU_mat_pastix(matPASTIX)
 
-      TYPE(PASTIX_STRUC) :: matPASTIX
+      TYPE(PASTIX_STRUC)        :: matPASTIX
 
-      real*8  :: tps, tpe
-      integer :: cks, clock_rate, cke
+      REAL*8                    :: tps, tpe
+      INTEGER                   :: cks, clock_rate, cke
       cks = 0.
 
-      if (lssolver%timing) then
-         call cpu_time(tps)
-         call system_clock(cks, clock_rate)
-      end if
+      IF (lssolver%timing) THEN
+         CALL cpu_time(tps)
+         CALL system_clock(cks, clock_rate)
+      END IF
 
-      call pastix_task_numfact(matPASTIX%pastix_data, matPASTIX%spm, matPASTIX%info)
-      if (matPASTIX%info .ne. 0) then
-         error stop "pastix_task_numfact failed"
-      end if
+      CALL pastix_task_numfact(matPASTIX%pastix_data, matPASTIX%spm, matPASTIX%info)
+      IF (matPASTIX%info .NE. 0) THEN
+         error STOP "pastix_task_numfact failed"
+      END IF
 
-      if (lssolver%timing) then
-         call cpu_time(tpe)
-         call system_clock(cke, clock_rate)
-         timing%rlstime5 = timing%rlstime5 + (cke - cks)/real(clock_rate)
+      IF (lssolver%timing) THEN
+         CALL cpu_time(tpe)
+         CALL system_clock(cke, clock_rate)
+         timing%rlstime5 = timing%rlstime5 + (cke - cks)/REAL(clock_rate)
          timing%clstime5 = timing%clstime5 + tpe - tps
-      end if
+      END IF
 
    END SUBROUTINE LU_mat_pastix
 
@@ -295,30 +295,30 @@ CONTAINS
    ! Solve problem with PASTIX
    !***********************************************
    SUBROUTINE solve_mat_PASTIX(matPASTIX)
-      TYPE(PASTIX_STRUC) :: matPASTIX
+      TYPE(PASTIX_STRUC)        :: matPASTIX
 
-      real*8  :: tps, tpe
-      integer :: cks, clock_rate, cke
+      REAL*8                    :: tps, tpe
+      INTEGER                   :: cks, clock_rate, cke
       cks = 0.
 
-      if (lssolver%timing) then
-         call cpu_time(tps)
-         call system_clock(cks, clock_rate)
-      end if
+      IF (lssolver%timing) THEN
+         CALL cpu_time(tps)
+         CALL system_clock(cks, clock_rate)
+      END IF
 
-      call pastix_task_solve_and_refine( matPASTIX%pastix_data, matPASTIX%spm%nexp, matPASTIX%nrhs, matPASTIX%b, matPASTIX%spm%nexp, matPASTIX%x, matPASTIX%spm%nexp, matPASTIX%info )
+      CALL pastix_task_solve_and_refine( matPASTIX%pastix_data, matPASTIX%spm%nexp, matPASTIX%nrhs, matPASTIX%b, matPASTIX%spm%nexp, matPASTIX%x, matPASTIX%spm%nexp, matPASTIX%info )
       ! call pastix_task_solve( matPASTIX%pastix_data, matPASTIX%spm%nexp, matPASTIX%nrhs, matPASTIX%x, matPASTIX%spm%nexp, matPASTIX%info )
-      if (matPASTIX%info .ne. 0) then
-         error stop "pastix_task_solve failed"
-      end if
+      IF (matPASTIX%info .NE. 0) THEN
+         error STOP "pastix_task_solve failed"
+      END IF
       matPASTIX%rhs = matPASTIX%x(:, 1)
 
-      if (lssolver%timing) then
-         call cpu_time(tpe)
-         call system_clock(cke, clock_rate)
-         timing%rlstime6 = timing%rlstime6 + (cke - cks)/real(clock_rate)
+      IF (lssolver%timing) THEN
+         CALL cpu_time(tpe)
+         CALL system_clock(cke, clock_rate)
+         timing%rlstime6 = timing%rlstime6 + (cke - cks)/REAL(clock_rate)
          timing%clstime6 = timing%clstime6 + tpe - tps
-      end if
+      END IF
    END SUBROUTINE solve_mat_PASTIX
 
    SUBROUTINE terminate_mat_PASTIX()
@@ -327,17 +327,17 @@ CONTAINS
       matPASTIX%spm%rowptr = c_null_ptr
       matPASTIX%spm%values = c_null_ptr
       matPASTIX%spm%loc2glob = c_null_ptr
-      call spmExit(matPASTIX%spm)
-      deallocate (matPASTIX%spm)
+      CALL spmExit(matPASTIX%spm)
+      DEALLOCATE (matPASTIX%spm)
 
-      if (allocated(matPASTIX%x)) then
-         deallocate (matPASTIX%x)
-         deallocate (matPASTIX%b)
-         deallocate (matPASTIX%rhs)
-      end if
+      IF (allocated(matPASTIX%x)) THEN
+         DEALLOCATE (matPASTIX%x)
+         DEALLOCATE (matPASTIX%b)
+         DEALLOCATE (matPASTIX%rhs)
+      END IF
 
       !  Destroy the C data structure (Should be last if used to call MPI_Finalize)
-      call pastixFinalize(matPASTIX%pastix_data)
+      CALL pastixFinalize(matPASTIX%pastix_data)
 
    END SUBROUTINE terminate_mat_PASTIX
 
